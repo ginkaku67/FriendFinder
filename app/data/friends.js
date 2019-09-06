314d52e2768d51a0c1026f3a48cd8e1213ea55d0
ginkaku67 <nshiply76@rocketmail.com>
2019-09-05 20:47:27 -0500

    66%

---
+++ app/data/friends.js
@@ -0,0 +1,5 @@
+currentPerson;
+previousSubmissions = [];
+
+(totalDifference)Math.abs(/*this is where the 2 values for potential friends go
+    it will have to be a for loop that runs through all the old submissions and compares them them to the new one.*/);
---
+++ server.js
@@ -0,0 +1,10 @@
+var express = require("express");
+var path = require("path");
+var app = express();
+var PORT = process.env.PORT || 3000;
+app.use(express.bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+//var friendsData = [];
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
