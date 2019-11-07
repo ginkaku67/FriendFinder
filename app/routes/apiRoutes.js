a264cc55dfd910ea629da8daa473d95d87fc2300
ginkaku67 <nshiply76@rocketmail.com>
2019-11-07 10:10:28 -0600

    no

--- app/routes/apiRoutes.js
+++ app/routes/apiRoutes.js
@@ -1,33 +1,30 @@
-var pBFF = require("../data/potentialBestFriendsForever");
+var potentialBestFriendsForever = require("../data/potentialBestFriendsForever");
 
 module.exports = function(app) {
 
     app.get("/api/potentialBestFriendsForever", function(req, res){
-        res.json(pBFF);
+        res.json(potentialBestFriendsForever);
     });
     app.post("/api/potentialBestFriendsForever", function(req, res){
-        console.log("POST request at /api/potentialBestFriendsForever");
         var user = req.body;
-        console.log(user);
         var bFF = {};
         var bestMatch = Infinity;
-        pBFF.forEach(function(friend) {
+        potentialBestFriendsForever.forEach(function(friend) {
             var cohesion = 0;
             for (let i=0; i<friend.scores.length; i++) {
             user.scores[i] = parseInt(user.scores[i]);
             cohesion += Math.abs(user.scores[i] - friend.scores[i]);
             }
-            console.log("compatibility: " + cohesion);
             if (cohesion<bestMatch) {
             bestMatch = cohesion;
             bFF = friend;
             }
         });
         res.json({
-            photo: bFF.photo,
-            name: bFF.name
+            name: bFF.name,
+            photo: bFF.photo
         });
 
-        pBFF.push(user)
+        potentialBestFriendsForever.push(user)
     });
 }
