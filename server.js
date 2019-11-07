88f39607cc51a67a7d7f26c131b5eb5ad3d0797d
ginkaku67 <nshiply76@rocketmail.com>
2019-11-07 17:33:08 -0600

    Tiny fixes

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ var bodyparser = require("body-parser")
 var PORT = process.env.PORT || 3000;
 app.use(bodyparser.urlencoded({ extended: true }));
 app.use(express.json());
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+require("./app/routes/apiRoutes")(app);
+require("./app/routes/htmlRoutes")(app);
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
