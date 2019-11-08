b605e40e606562c697781c84e26e444e217647ae
ginkaku67 <nshiply76@rocketmail.com>
2019-11-08 08:32:58 -0600

    console now shows user data is being captured

--- app/routes/htmlRoutes.js
+++ app/routes/htmlRoutes.js
@@ -1,4 +1,4 @@
-var path = require("path");
+/*var path = require("path");
 module.exports = function(app) {
 
   app.get("/survey", function(req, res) {
@@ -7,4 +7,21 @@ module.exports = function(app) {
   app.get("*", function(req, res) {
     res.sendFile(path.join(__dirname, "/../public/home.html"));
   });
+}*/
+  
+var path = require("path");
+
+module.exports = function (app) {
+
+    //Routes to question
+    app.get("/survey", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/survey.html"));
+    });
+
+    //routes home if any other url route is passed
+    app.get("*", function (req, res) {
+        app.get("/survey", function (req, res) {
+            res.sendFile(path.join(__dirname, "app/public/home.html"));
+        });
+    });
 }
